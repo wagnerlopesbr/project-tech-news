e4f002f0b6a8acd38b26585cc48946c5f17959e1
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-06-25 11:17:26 -0300

    WL - Project Tech News - Req 9/9 v1

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -24,5 +24,9 @@ def search_by_date(date):
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    search = search_news({
+        "category": {"$regex": category, "$options": "i"}
+        })
+    if not search:
+        return []
+    return [(news["title"], news["url"]) for news in search]
