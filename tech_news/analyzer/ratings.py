b41ded86b3b9090c271917977d4629a3ffaf1249
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-06-25 12:07:43 -0300

    WL - Project Tech News - Req 10/9 v1 - BONUS

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,17 @@
+from tech_news.database import db
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    pipeline = [
+        {"$unwind": "$category"},
+        {"$group": {"_id": "$category", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1, "_id": 1}},
+        {"$limit": 5}
+    ]
+    
+    categories = list(db.news.aggregate(pipeline))
+    
+    top_categories = [category["_id"] for category in categories]
+    
+    return top_categories
